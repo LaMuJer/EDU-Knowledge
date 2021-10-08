2e9452c5ada045f702ca4dcf3853488100c87b7d
laMuJer <lareina1467.com>
2021-10-09 00:19:35 +0630

    landing page

---
+++ main.js
@@ -0,0 +1,8 @@
+// loading section starts 
+    $(window).on("load" , () => {
+        $('#loader').fadeOut(500 , () => {
+        $(this).remove()
+        });
+        // document.getElementById('loader').style.zIndex = 100;
+    });
+// loading section ends
