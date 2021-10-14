7ef5ef0c6a29faa2301bbe276715d56688153adc
laMuJer <lareina1467.com>
2021-10-15 00:16:39 +0630

    footer

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 // loading section starts 
     $(window).on("load" , () => {
         $('#loader').fadeOut(500 , () => {
-        $(this).remove()
+        // $(this).remove()
         });
         // document.getElementById('loader').style.zIndex = 100;
     });
 // loading section ends
+
